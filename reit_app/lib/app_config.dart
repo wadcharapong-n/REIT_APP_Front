aadbc08a53603c73ef0f041ad7715ba4d20b49df
Champ Kitphon <kitphon@odds.team>
2019-04-29 22:20:49 +0700

    edit app config

--- reit_app/lib/app_config.dart
+++ reit_app/lib/app_config.dart
@@ -1,7 +1,7 @@
 class AppConfig {
-  // static String apiUrl = "https://reit-api.odds.team/api";
-  // static String authApiUrl = "https://reit-api.odds.team/Auth";
+  static String apiUrl = "https://reit-api.odds.team/api";
+  static String authApiUrl = "https://reit-api.odds.team/Auth";
   static String googleApiKey = "AIzaSyCD6fMRbaD5XE3ZbYsfVryOMxY-0viOk8Y";
-  static String apiUrl = "http://10.0.2.2:1323/api";
-  static String authApiUrl = "http://10.0.2.2:1323/Auth";
+  // static String apiUrl = "http://10.0.2.2:1323/api";
+  // static String authApiUrl = "http://10.0.2.2:1323/Auth";
 }

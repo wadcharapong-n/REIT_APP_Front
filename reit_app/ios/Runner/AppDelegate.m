82d2e31de760ce9228290db86c6b40f6b952a611
Champ Kitphon <kitphon@odds.team>
2019-05-07 20:43:28 +0700

    try to register facebook login in AppDelegate

--- reit_app/ios/Runner/AppDelegate.m
+++ reit_app/ios/Runner/AppDelegate.m
@@ -28,4 +28,29 @@
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
+//  AppDelegate.m
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+
+- (BOOL)application:(UIApplication *)application 
+    didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+  
+  [[FBSDKApplicationDelegate sharedInstance] application:application
+    didFinishLaunchingWithOptions:launchOptions];
+  // Add any custom logic here.
+  return YES;
+}
+
+- (BOOL)application:(UIApplication *)application 
+            openURL:(NSURL *)url 
+            options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
+
+  BOOL handled = [[FBSDKApplicationDelegate sharedInstance] application:application
+    openURL:url
+    sourceApplication:options[UIApplicationOpenURLOptionsSourceApplicationKey]
+    annotation:options[UIApplicationOpenURLOptionsAnnotationKey]
+  ];
+  // Add any custom logic here.
+  return handled;
+}
+
 @end

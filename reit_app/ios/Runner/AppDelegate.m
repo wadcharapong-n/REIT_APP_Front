9e40b0b223b91156bdbb2259d76e1f0276dc2d36
v_wadcha <v_wadcha@ctlsd.set>
2019-05-07 20:44:15 +0700

    edit config

--- reit_app/ios/Runner/AppDelegate.m
+++ reit_app/ios/Runner/AppDelegate.m
@@ -1,25 +1,8 @@
 #include "AppDelegate.h"
 #include "GeneratedPluginRegistrant.h"
-
-#include "AppDelegate.h"
-#include "GeneratedPluginRegistrant.h"
 #import "GoogleMaps/GoogleMaps.h"
 
 @implementation AppDelegate
-
-// - (BOOL)application:(UIApplication *)application
-//     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-//   // Provide the GoogleMaps API key.
-//   NSString* mapsApiKey = [[NSProcessInfo processInfo] environment][@"AIzaSyCD6fMRbaD5XE3ZbYsfVryOMxY-0viOk8Y"];
-//   if ([mapsApiKey length] == 0) {
-//     mapsApiKey = @"AIzaSyCD6fMRbaD5XE3ZbYsfVryOMxY-0viOk8Y";
-//   }
-//   [GMSServices provideAPIKey:mapsApiKey];
-  
-//   [GeneratedPluginRegistrant registerWithRegistry:self];
-//   // Override point for customization after application launch.
-//   return [super application:application didFinishLaunchingWithOptions:launchOptions];
-// }
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
